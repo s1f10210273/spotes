f7ec4827d719117483dadd9f9b90064f9b43c46a
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2023-06-01 03:28:30 +0900

    aaa

--- spotes/urls.py
+++ spotes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.index, name="index"),
-    path("login/", views.spotify_login, name="login"),
-    path("logout/", views.spotify_logout, name="logout"),
-    path("callback/", views.callback, name="callback"),
-    path("home/", views.home, name="home"),
+    path("", views.index, name="index"),
+    path("login", views.spotify_login, name="login"),
+    path("logout", views.spotify_logout, name="logout"),
+    path("callback", views.callback, name="callback"),
+    path("home", views.home, name="home"),
 ]
