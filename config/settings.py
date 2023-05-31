3735cf70ea8c75542be55b378b0f2961d4ffe90a
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2023-06-01 04:06:50 +0900

    aa

--- config/settings.py
+++ config/settings.py
@@ -79,12 +79,6 @@ DATABASES = {
     }
 }
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
 
 
 
