1d830f1baaee9e82793596c4016a72f2dedb6c68
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2023-06-01 03:39:26 +0900

    あああ

--- spotes/views.py
+++ spotes/views.py
@@ -41,7 +41,7 @@ def callback(request):
     params = {
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': "https://spoti-quct.onrender.com/spotes/home/",
+        'redirect_uri': SPOTIFY_REDIRECT_URI,
         'client_id': SPOTIFY_CLIENT_ID,
         'client_secret': SPOTIFY_CLIENT_SECRET,
     }
