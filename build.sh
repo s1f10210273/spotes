658d0c139dc9c1a9bffd0c174beb72a72173d964
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2023-06-01 03:59:23 +0900

    データベースうざ

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-python manage.py makemigrations
-python manage.py migrate
 
 pip install -r requirements.txt
--- config/settings.py
+++ config/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
+
 
 
 # Password validation
