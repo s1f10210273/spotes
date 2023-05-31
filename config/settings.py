85198389dd0cae7a6d562bb30b30800e55a57c99
s1f10210273 <85671747+s1f10210273@users.noreply.github.com>
2023-06-01 03:52:00 +0900

    aa

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
